8230be24d7e8a1338c920f6b2cca0a37e3c78400
Quebin <matsuda.m.kevin@gmail.com>
2022-06-14 20:45:41 -0300

    frontend hardcode
    front end hardcoreado, hay q bindear la data

--- frontend-matsuda/src/app/app.module.ts
+++ frontend-matsuda/src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { ExpComponent } from './componentes/exp/exp.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    EncabezadoComponent,
+    PerfilComponent,
+    ExpComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
